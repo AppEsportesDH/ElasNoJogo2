c94c097aeda739ed8e0c89d40c9097de6b2bdd35
Sol Falcão <silmarasol@hotmail.com>
2020-04-15 18:53:07 -0300

    Débitos técnicos Sol - Renomear todos os pacotes para letras minúsculas - Implementar a exibição das categorias vindo da API, pois hoje a aplicação não exibe os dados - Alterar a cor da font para inserção de texto na tela de cadastro pois não dá pra ler após inserir um valor no campo - Padronizar os espaços dos componentes de view na tela de login e cadastro. Adicionar uma margem de 16dp tanto left como right - Fazer o tratamento do erro na camada da view, ou remover o atributo que nao está sendo usado na classe SportViewModel

---
+++ app/src/main/java/br/com/elasnojogo/views/PesquisaFragment.java
@@ -0,0 +1,66 @@
+package br.com.elasnojogo.views;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.elasnojogo.views.interfaces.FavoritosView;
+import br.com.elasnojogo.model.DadosEvento;
+import br.com.elasnojogo.views.adapter.FavoritoRecyclerViewAdapter;
+import br.com.elasnojogo2.R;
+
+import static br.com.elasnojogo.constantes.Constantes.EVENTO_CHAVE;
+
+public class PesquisaFragment extends Fragment implements FavoritosView {
+
+    private RecyclerView recyclerViewFavorito;
+    private FavoritoRecyclerViewAdapter adapter;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_pesquisa, container, false);
+
+        recyclerViewFavorito = view.findViewById(R.id.recycler_view_favoritos);
+        adapter = new FavoritoRecyclerViewAdapter(getListaEventos(), this);
+        recyclerViewFavorito.setAdapter(adapter);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false);
+        recyclerViewFavorito.setLayoutManager(layoutManager);
+
+        return view;
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
+    }
+
+    private List<DadosEvento> getListaEventos() {
+        List<DadosEvento> eventos = new ArrayList<>();
+
+        eventos.add(new DadosEvento(R.drawable.futebol,"Fut das Migas", "Local: Digital House, SP", "Data: 20/10/2021"));
+        eventos.add(new DadosEvento(R.drawable.volei,"Liga de Volei Feminino", "Local: Avenida Paulista, 123", "Data: 21/01/2022"));
+        eventos.add(new DadosEvento(R.drawable.corrida,"Corrida na ZN", "Local: Avenida do Estado", "Data: 22/02/2023"));
+
+        return eventos;
+    }
+
+    @Override
+    public void visualizarEvento(DadosEvento dadosEvento) {
+        Fragment fragment = new VisualizarEvento();
+        Bundle bundle = new Bundle();
+        bundle.putParcelable(EVENTO_CHAVE, dadosEvento);
+        fragment.setArguments(bundle);
+
+        replaceFragment(fragment);
+    }
+}
