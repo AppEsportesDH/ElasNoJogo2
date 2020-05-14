a057cdd417342b9e6fe6b8bf3c7b92326fea60fb
cbalestieri <camille.balestieri@gmail.com>
2020-05-13 21:25:27 -0300

    Alterações na homeactivity para puxar as informações do google

--- app/src/main/java/br/com/elasnojogo/views/HomeActivity.java
+++ app/src/main/java/br/com/elasnojogo/views/HomeActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.ActionBarDrawerToggle;
@@ -14,13 +15,16 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.fragment.app.Fragment;
 import androidx.appcompat.widget.Toolbar;
 
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.squareup.picasso.Picasso;
 
 import br.com.elasnojogo2.R;
 
 import static br.com.elasnojogo.constantes.Constantes.USUARIO;
+import static br.com.elasnojogo.views.LoginActivity.GOOGLE_ACCOUNT;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -50,9 +54,19 @@ public class HomeActivity extends AppCompatActivity {
         TextView nav_user = emailText.findViewById(R.id.textViewEmail);
         nav_user.setText(user.getDisplayName());
 
+
         mAppBarConfiguration = new AppBarConfiguration.Builder(R.id.nav_home, R.id.nav_meus_eventos,  R.id.nav_editarPerfil, R.id.nav_sair).setDrawerLayout(drawer).build();
 
         navigationView.setNavigationItemSelectedListener(this::onNavigationItemSelected);
+
+        View hview = navigationView.getHeaderView(0);
+        TextView nav_email = hview.findViewById(R.id.textViewEmail);
+        TextView nav_name = hview.findViewById(R.id.nome_usuaria_drawer);
+        ImageView nav_img = hview.findViewById(R.id.nav_imageView);
+        GoogleSignInAccount googleSignInAccount = getIntent().getParcelableExtra(GOOGLE_ACCOUNT);
+        Picasso.get().load(googleSignInAccount.getPhotoUrl()).centerInside().fit().into(nav_img);
+        nav_name.setText(googleSignInAccount.getDisplayName());
+        nav_email.setText(googleSignInAccount.getEmail());
     }
 
     private boolean onNavigationItemSelected(MenuItem menuItem) {
